0811d8e8b2ec1ffb48b2b89ec0f01fecfac4f1a0
ngochoanit <phamngochoan.it@gmail.com>
2021-10-15 13:44:27 +0700

    update

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,9 +1,13 @@
 import express from 'express'
+import { HomePageController } from '../controllers/HomeController'
 let router = express.Router()
 let initWebRoutes = (app) => {
-    router.get('/', (req, res) => {
-        return res.send("hello world")
-    })
+    router.get('/', HomePageController.getHomePage)
+
+    // Creates the endpoint for our webhook 
+    app.post('/webhook', HomePageController.postWebhook);
+    // Adds support for GET requests to our webhook
+    app.get('/webhook', HomePageController.getWebhook)
     return app.use('/', router)
 }
 module.exports = initWebRoutes
